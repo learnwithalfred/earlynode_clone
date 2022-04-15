3a0becc9ab62a3649a1529c2b0ddc9a30a54bca9
Alfred Boateng <programmingwithalfred@gmail.com>
2022-04-15 14:35:33 +0000

    Add JSON Server files
     -  Data added to db.json for testing
     - index file renders the json server

---
+++ index.js
@@ -0,0 +1,11 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+
+server.use(router)
+
+const port = process.env.PORT || 5000
+
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`)
+})
